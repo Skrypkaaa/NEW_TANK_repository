ab421c046f2983a7a9bcc5023ab364cf7fad81cf
NatNovs <136241094+NatNovs@users.noreply.github.com>
2023-12-01 12:48:12 +0100

    footer-3
    js

--- src/js/select.js
+++ src/js/select.js
@@ -1,1 +1,25 @@
 
+const MenuOption = document.querySelector('.custom-select-ctg');
+
+const MenuSopts = document.querySelector('.custom-select-sort');
+
+document.addEventListener('click', function (event) {
+    const isClickInsideMenuOption = MenuOption.contains(event.target);
+    const isClickInsideMenuSopts = MenuSopts.contains(event.target);
+
+    if (!isClickInsideMenuOption) {
+        MenuOption.classList.remove('active');
+    }
+
+    if (!isClickInsideMenuSopts) {
+        MenuSopts.classList.remove('active');
+    }
+    });
+    
+    document.addEventListener('keydown', function (event) {
+    if (event.code === 'Escape') {
+        MenuOption.classList.remove('active');
+        MenuSopts.classList.remove('active');
+    }
+    });
+    
