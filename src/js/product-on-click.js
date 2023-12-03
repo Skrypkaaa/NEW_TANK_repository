d111e511a4266bf76f94afef125c9bac51b79c11
YaroslavKyba <narxoc.ne@gmail.com>
2023-12-03 02:52:26 +0200

    changes

--- src/js/product-on-click.js
+++ src/js/product-on-click.js
@@ -1,20 +1,18 @@
-import { markupProductsList } from './product-list';
 import { openModal } from './modal-markup';
-import { ShopStorage } from './local-storage';
 
 const listEl = document.querySelector('.content-wrapper');
 listEl.addEventListener('click', onClickCart);
 
 export function onClickCart(e) {
-    e.preventDefault();
-    const clickedElement = e.target;
-    if (
-        clickedElement.closest('a') &&
-        clickedElement.closest('.products-card-link')
-    ) {
-        const id = clickedElement.closest('li').dataset.id;
-        openModal(id);
-    } else {
-        return;
-    }
+  e.preventDefault();
+  const clickedElement = e.target;
+  if (
+    clickedElement.closest('a') &&
+    clickedElement.closest('.products-card-link')
+  ) {
+    const id = clickedElement.closest('li').dataset.id;
+    openModal(id);
+  } else {
+    return;
+  }
 }
