a1b20482f239715292bf9203089bcab9e1282d33
SerhiiAnd <136363351+SerhiiAnd@users.noreply.github.com>
2023-12-03 22:12:46 +0100

    last-save123

--- src/js/cart/cart-product-markup.js
+++ src/js/cart/cart-product-markup.js
@@ -9,11 +9,16 @@ export function cartProductMarkup(arr) {
                             <p class="cart-product-price">$${price}</p>
                         </div>
                         <button type="button" class="cart-product-close-btn">
-                            <svg class="cart-icon-close">
-                                <use href="./img/icons-tank.svg#icon-close"></use>
-                            </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+<path d="M15.625 5.6832L14.3168 4.375L10 8.6918L5.6832 4.375L4.375 5.6832L8.6918 10L4.375 14.3168L5.6832 15.625L10 11.3082L14.3168 15.625L15.625 14.3168L11.3082 10L15.625 5.6832Z" fill="#010101"/>
+</svg>
+                          
                     </button>
                     </li>`;
     })
     .join('');
 }
+
+// <svg class="cart-icon-close">
+//   <use href="./img/icons-tank.svg#icon-close"></use>
+// </svg>;
