4a69875014e4374ce5d8b149cf20555366384edf
SerhiiAnd <136363351+SerhiiAnd@users.noreply.github.com>
2023-12-03 04:38:38 +0100

    svg

--- src/js/buttons-markup.js
+++ src/js/buttons-markup.js
@@ -1,44 +1,44 @@
-import icons from '../img/icons-tank.svg'
+import icons from '../img/icons-tank.svg';
 
 export class PaginationButtons {
-    constructor() {}
-    numBtnCreateMarkUp(num) {
-        return `<li class="buttons-item">
+  constructor() {}
+  numBtnCreateMarkUp(num) {
+    return `<li class="buttons-item">
             <button class="pagin-btn num-btn" type="button" data-button-id="${num}">${num}</button>
         </li>`;
-    }
-    numBtnSetActive(num) {}
-    numBtnSetInactive(num) {}
-    numBtnChangeNum() {}
+  }
+  numBtnSetActive(num) {}
+  numBtnSetInactive(num) {}
+  numBtnChangeNum() {}
 
-    intervalBtnCreateMarkUp() {
-        return `<li class="buttons-item">
+  intervalBtnCreateMarkUp() {
+    return `<li class="buttons-item">
             <button class="pagin-btn interval-btn" type="button" data-button-id="interval">...</button>
         </li>`;
-    }
-    intervalBtnShow() {}
-    intervalBtnHide() {}
+  }
+  intervalBtnShow() {}
+  intervalBtnHide() {}
 
-    prevBtnCreateMarkUp() {
-        return `<li class="buttons-item">
+  prevBtnCreateMarkUp() {
+    return `<li class="buttons-item">
             <button class="pagin-btn prev-btn" type="button" data-button-id="prev-btn" aria-label="Previous">
                 <svg class="icon-shopping-cart" width="24" height="24">
-                    <use href="${icons}#icon-left-side" class="pagination-arrows" />
+                    <use href="${icons}#icon-caret-small-left" class="pagination-arrows" />
                 </svg>
             </button>
         </li>`;
-    }
-    nextBtnCreateMarkUp() {
-        return `<li class="buttons-item">
+  }
+  nextBtnCreateMarkUp() {
+    return `<li class="buttons-item">
             <button class="pagin-btn next-btn" type="button" data-button-id="next-btn" aria-label="Next">
                 <svg class="icon-shopping-cart" width="24" height="24">
-                    <use href="${icons}#icon-right-side" class="pagination-arrows" />
+                    <use href="${icons}#icon-caret-small-right" class="pagination-arrows" />
                 </svg>
             </button>
         </li>`;
-    }
-    prevBtnSetActive(num) {}
-    prevBtnSetInactive(num) {}
-    nextBtnSetActive(num) {}
-    nextBtnSetInactive(num) {}
+  }
+  prevBtnSetActive(num) {}
+  prevBtnSetInactive(num) {}
+  nextBtnSetActive(num) {}
+  nextBtnSetInactive(num) {}
 }
